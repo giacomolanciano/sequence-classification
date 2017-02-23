83823fed0c0defc0296473ca7966ff839ac1349a
Giacomo <g.lanciano1@gmail.com>
2017-02-23 18:00:57 +0100

    add unique labels counting

--- utils/persistence.py
+++ utils/persistence.py
@@ -3,7 +3,12 @@ from utils.constants import DATABASE
 
 
 def insert_protein(pdb_id, sequence, class_label):
-    """ Insert protein data into db. """
+    """
+    Insert protein data into db.
+    :param pdb_id: protein PDB identifier.
+    :param sequence: protein amino acids sequence.
+    :param class_label: protein classification.
+    """
     connection = sqlite3.connect(DATABASE)
     cursor = connection.cursor()
     try:
@@ -15,7 +20,11 @@ def insert_protein(pdb_id, sequence, class_label):
 
 
 def is_known_protein(pdb_id):
-    """ Tells whether a protein is already in db or not. """
+    """
+    Tells whether a protein is already in db or not.
+    :param pdb_id: protein PDB identifier.
+    :return: True if already in, False otherwise.
+    """
     connection = sqlite3.connect(DATABASE)
     cursor = connection.cursor()
     cursor.execute('''SELECT * FROM protein WHERE pdb_id = ?''', (pdb_id,))
@@ -24,6 +33,22 @@ def is_known_protein(pdb_id):
     connection.commit()
     connection.close()
     return False
+
+
+def get_proteins_unique_labels():
+    """
+    Count how many distinct proteins labels are stored in db.
+    :return: The number of distinct labels.
+    """
+    result = 0
+    connection = sqlite3.connect(DATABASE)
+    cursor = connection.cursor()
+    cursor.execute('''SELECT COUNT(DISTINCT class_label) FROM protein''')
+    for row in cursor:
+        result = row[0]
+    connection.commit()
+    connection.close()
+    return result
 
 
 if __name__ == '__main__':
@@ -41,10 +66,10 @@ if __name__ == '__main__':
     # c.execute("DELETE FROM protein")
 
     # Show  tables
-    print('\nprotein')
-    c.execute("SELECT * FROM protein")
-    for row in c:
-        print(row)
+    # print('\nprotein')
+    # c.execute("SELECT * FROM protein")
+    # for row in c:
+    #     print(row)
 
     # Save (commit) the changes
     conn.commit()
