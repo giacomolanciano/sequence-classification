43ec95ade91f54a02ae1ed3f22c7c3a111b9c388
Giacomo <g.lanciano1@gmail.com>
2017-02-28 11:15:16 +0100

    add pdb website fastas crawling

--- Crawling/crawling.py
+++ Crawling/crawling.py
@@ -8,6 +8,7 @@ import utils.persistence as persistence
 from utils.constants import DATA_FOLDER
 
 TABLE_PDB_FILE = os.path.join(DATA_FOLDER, 'Table_PDB_Domain.tsv')
+PDB_FILE = os.path.join(DATA_FOLDER, 'pdb_seqres.txt')
 FASTA_URL = 'http://www.rcsb.org/pdb/files/fasta.txt?structureIdList='
 CLASS_URL = 'http://www.rcsb.org/pdb/explore/explore.do?structureId='
 PDB_ID_HEADER = 'PDB'
@@ -55,10 +56,54 @@ class Crawler(object):
     def _parse_protein_sequence(fasta_sequence):
         return fasta_sequence.split('>')[1].split('\n', maxsplit=1)[1].replace('\n', '')
 
+    def build_proteins_dataset_local(self, data_path):
+        with open(data_path, 'r') as file:
+
+            i = 0
+            header_line = file.readline()
+            sequence_line = file.readline()
+
+            while header_line is not '':
+                pdb_id = Crawler._parse_protein_pdb_id(header_line)
+
+                if self.progress:
+                    print(i)
+                    print(pdb_id)
+
+                # check whether protein is already in db to avoid costly network operations
+                if not persistence.is_known_protein(pdb_id):
+                    try:
+
+                        # download class label corresponding to sequence
+                        class_http_request = requests.get(CLASS_URL + pdb_id)
+                        soup = bs(class_http_request.text, 'lxml')
+                        class_label = soup.find('ul', attrs={'class': 'list-unstyled'}).find('li').find('a')\
+                            .get_text().strip()
+
+                        # insert protein data in db
+                        persistence.insert_protein(pdb_id, sequence_line, class_label)
+
+                    except (TimeoutError, ConnectionError) as err:
+                        # if network error occurs, skip
+                        if self.progress:
+                            print(err)
+                        self.errored_pdb_id.append(pdb_id)
+                        continue
+
+                i += 1
+                header_line = file.readline()
+                sequence_line = file.readline()
+
+    @staticmethod
+    def _parse_protein_pdb_id(header):
+        return str(header[1:5]).upper()
 
 if __name__ == '__main__':
     c = Crawler()
-    c.build_proteins_dataset(TABLE_PDB_FILE)
+
+    # c.build_proteins_dataset(TABLE_PDB_FILE)
+
+    c.build_proteins_dataset_local(PDB_FILE)
     if c.errored_pdb_id:
         print('errored pdb_ids: ' + str(c.errored_pdb_id))
     print('unique labels: ' + str(persistence.get_proteins_unique_labels()))
