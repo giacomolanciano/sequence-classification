066ab08d4f52afa2ff9a2821ea562fa47580c486
Giacomo <g.lanciano1@gmail.com>
2017-03-14 12:38:14 +0100

    rename modules

--- utils/constants.py
+++ utils/constants.py
@@ -1,7 +1,7 @@
 """ Module containing all constants. """
 import os
 
-DATA_FOLDER = os.path.join(os.path.dirname(os.path.dirname(os.path.abspath(__file__))), 'Data')
+DATA_FOLDER = os.path.join(os.path.dirname(os.path.dirname(os.path.abspath(__file__))), 'data')
 DATABASE = os.path.join(DATA_FOLDER, 'proteins.db')
 
 
