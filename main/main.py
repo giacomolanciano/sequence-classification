1bbda5cc0ed98d9220bebc07489b4750e7da06c1
Giacomo <g.lanciano1@gmail.com>
2017-03-14 12:35:01 +0100

    refactor SVM main

--- main/main.py
+++ main/main.py
@@ -13,11 +13,13 @@ start_time = time.time()
 clf.fit(my_ml_input.train_data, np.asarray(my_ml_input.train_labels))
 accuracy = clf.score(my_ml_input.test_data, my_ml_input.test_labels)
 elapsed = (time.time() - start_time)
+
 if elapsed < 60:
     print('Time: {:3.2f} seconds' .format(elapsed))
-elif (elapsed >= 60) and (elapsed <3600):
+elif (elapsed >= 60) and (elapsed < 3600):
     print('Time: {:3.2f} minutes'.format(elapsed/60))
 else:
     print('Time: {:3.2f} hours'.format(elapsed / 3600))
+
 print('Accuracy: {:3.2f}%'.format(100 * accuracy))
 
