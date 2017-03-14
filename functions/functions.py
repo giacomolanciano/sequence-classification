8f0d9c828384918ef3cdb95466121652bb070103
Luca Marchetti <marchetti.luca.1992@gmail.com>
2017-03-14 12:41:14 +0100

    refactoring

--- functions/functions.py
+++ functions/functions.py
@@ -15,7 +15,6 @@ def p_spectrum_kernel_function(string1, string2):
                         kernel += 1
             row.append(kernel)
         kernel_matrix.append(row)
-    #print(kernel_matrix)
     return kernel_matrix
 
 
