7e05fe099148b45a7f157ff6d0c827b957f3da80
Giacomo <g.lanciano1@gmail.com>
2017-03-17 17:52:58 +0100

    add cross validation to svm main

--- main/svm_main.py
+++ main/svm_main.py
@@ -1,4 +1,6 @@
 from sklearn import svm
+from sklearn import model_selection
+from sklearn.model_selection import StratifiedKFold
 import numpy as np
 import time
 
@@ -6,26 +8,38 @@ from machine_learning.sequence_classifier import occurrence_dict_spectrum_kernel
 from machine_learning.sequence_classifier import SequenceClassifierInput
 
 
-clf_input = SequenceClassifierInput(inputs_per_label=100)  # to speed up testing
+def print_time(seconds):
+    result = '\n'
+    if seconds < 60:
+        result += 'Time: {:3.2f} seconds'.format(seconds)
+    elif (seconds >= 60) and (seconds < 3600):
+        result += 'Time: {:3.2f} minutes'.format(seconds / 60)
+    else:
+        result += 'Time: {:3.2f} hours'.format(seconds / 3600)
+    print(result)
+
+
+clf = svm.SVC(kernel='precomputed')
+
+# build training and test splits
+clf_input = SequenceClassifierInput(inputs_per_label=100)
 clf_input.set_train_test_data(['TRANSCRIPTION', 'LYASE'])
 
-clf = svm.SVC(kernel=occurrence_dict_spectrum_kernel)
-
-# from sklearn import model_selection
-# d = {'C': [1, 10]}
-# gd = model_selection.GridSearchCV(d, clf)
-
+# merge splits (cross validation and folding will be performed)
+inputs_data = clf_input.train_data + clf_input.test_data
+inputs_labels = np.asarray(clf_input.train_labels + clf_input.test_labels)
 
 start_time = time.time()
-clf.fit(clf_input.train_data, np.asarray(clf_input.train_labels))
-accuracy = clf.score(clf_input.test_data, clf_input.test_labels)
+
+# pre-compute kernel matrix
+kernel_matrix_train = np.asarray(occurrence_dict_spectrum_kernel(inputs_data, inputs_data))
+
+# cross validation
+param_grid = {'C': [1, 10]}
+gd = model_selection.GridSearchCV(clf, param_grid=param_grid, cv=StratifiedKFold(n_splits=10))
+gd.fit(kernel_matrix_train, inputs_labels)
+print('Best params:   %s' % gd.best_params_)
+print('Best accuracy: {:3.2f}%'.format(100 * gd.best_score_))
+
 elapsed = (time.time() - start_time)
-
-if elapsed < 60:
-    print('Time: {:3.2f} seconds' .format(elapsed))
-elif (elapsed >= 60) and (elapsed < 3600):
-    print('Time: {:3.2f} minutes'.format(elapsed/60))
-else:
-    print('Time: {:3.2f} hours'.format(elapsed / 3600))
-
-print('Accuracy: {:3.2f}%'.format(100 * accuracy))
+print_time(elapsed)
