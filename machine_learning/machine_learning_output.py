abccb35cda0165d895d2d0f9bcc7bf13c78007a2
Luca Marchetti <marchetti.luca.1992@gmail.com>
2017-03-22 13:02:01 +0100

    some nd modification

--- machine_learning/machine_learning_output.py
+++ machine_learning/machine_learning_output.py
@@ -15,39 +15,41 @@ class MachineLearningOutput():
         self.confusion_matrix = confusion_matrix(self.y_test, self.y_prediction)
 
 
-    @staticmethod #cm = confusion_matrix
-    def _plot_confusion_matrix(cm, classes,
-                              normalize=False,
+    #cm = confusion_matrix
+    def plot_confusion_matrix(self,
+                              normalize=True,
                               title='Confusion matrix',
                               cmap=plt.cm.Blues):
         """
         This function prints and plots the confusion matrix.
         Normalization can be applied by setting `normalize=True`.
         """
-        plt.imshow(cm, interpolation='nearest', cmap=cmap)
+        plt.imshow(self.confusion_matrix, interpolation='nearest', cmap=cmap)
         plt.title(title)
         plt.colorbar()
-        tick_marks = np.arange(len(classes))
-        plt.xticks(tick_marks, classes, rotation=45)
-        plt.yticks(tick_marks, classes)
+        tick_marks = np.arange(len(self.class_names))
+        plt.xticks(tick_marks, self.class_names, rotation=45)
+        plt.yticks(tick_marks, self.class_names)
 
         if normalize:
-            cm = cm.astype('float') / cm.sum(axis=1)[:, np.newaxis]
+            self.confusion_matrix = self.confusion_matrix.astype('float') / self.confusion_matrix.sum(axis=1)[:, np.newaxis]
+            self.confusion_matrix = np.around(self.confusion_matrix,decimals=2)
             print("Normalized confusion matrix")
         else:
             print('Confusion matrix, without normalization')
 
-        print(cm)
+        print(self.confusion_matrix)
 
-        thresh = cm.max() / 2.
-        for i, j in itertools.product(range(cm.shape[0]), range(cm.shape[1])):
-            plt.text(j, i, cm[i, j],
+        thresh = self.confusion_matrix.max() / 2.
+        for i, j in itertools.product(range(self.confusion_matrix.shape[0]), range(self.confusion_matrix.shape[1])):
+            plt.text(j, i, self.confusion_matrix[i, j],
                      horizontalalignment="center",
-                     color="white" if cm[i, j] > thresh else "black")
+                     color="white" if self.confusion_matrix[i, j] > thresh else "black")
 
         plt.tight_layout()
         plt.ylabel('True label')
         plt.xlabel('Predicted label')
+        plt.show()
 
 
 
--- main/svm_main.py
+++ main/svm_main.py
@@ -5,15 +5,18 @@ from sklearn import model_selection
 import numpy as np
 import time
 
-from machine_learning.kernel_functions import precomputed_occurrence_dict_spectrum_kernel
+from machine_learning.kernel_functions import precomputed_occurrence_dict_spectrum_kernel, \
+    occurrence_dict_spectrum_kernel
+from machine_learning.machine_learning_output import MachineLearningOutput
 from machine_learning.sequence_classifier import SequenceClassifierInput
 
+class_names = ['OXIDOREDUCTASE', 'PROTEIN TRANSPORT', 'LECTIN'] #OXIDOREDUCTASE,,TRANSCRIPTION
 
 clf = svm.SVC(kernel='precomputed')
 
 # build training and test splits
-clf_input = SequenceClassifierInput(inputs_per_label=1000)
-clf_input.set_train_test_data(['TRANSCRIPTION', 'LYASE'])
+clf_input = SequenceClassifierInput(inputs_per_label=100)
+clf_input.set_train_test_data(class_names)
 
 # merge splits (cross validation and folding will be performed)
 inputs_data = clf_input.train_data + clf_input.test_data
@@ -35,3 +38,10 @@ elapsed_time = (time.time() - start_time)
 print('Best params:   %s' % grid.best_params_)
 print('Best accuracy: {:3.2f}%'.format(100 * grid.best_score_))
 print('Time: ', timedelta(seconds=elapsed_time))
+
+clf = svm.SVC(kernel=occurrence_dict_spectrum_kernel, C=grid.best_params_['C'] )
+clf.fit(clf_input.train_data,clf_input.train_labels)
+y_pred = clf.predict(clf_input.test_data)
+confiusion_matrix = MachineLearningOutput(clf_input.test_labels,y_pred,class_names)
+confiusion_matrix.get_confusion_matrix()
+confiusion_matrix.plot_confusion_matrix()
